643f41e97c3913fe5e064d4fbc57dd90d434f376
qbobl5 <ddddd991125@gmail.com>
2024-08-21 16:06:04 +0900

    [fix] 좌석 기능 수정 및 main merge

--- src/main/java/dev/movie/Main.java
+++ src/main/java/dev/movie/Main.java
@@ -8,10 +8,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Console.writeln("🎬 어서오세요! <우리 극장>입니다! 🎬");
-		
+
 		String movieName = MainController.showMovieList();
 		Long movieId = MainController.showTimeList(movieName);
 		SelectedSeat movieSeat = MainController.showSeatList(movieId);
+		
 	}
 
 }
