b8e741812ec80698848f59a422d42f1a82f2f24f
qbobl5 <113408131+qbobl5@users.noreply.github.com>
2024-08-21 15:57:19 +0900

    Update PayService.java

--- src/main/java/dev/movie/service/PayService.java
+++ src/main/java/dev/movie/service/PayService.java
@@ -3,10 +3,17 @@ package dev.movie.service;
 import dev.service.cloud.Console;
 
 public class PayService {
-	public int payByGift(int price) {
+	private static int payPrice(int sale, int priceRow) {
+		int payPrice = priceRow;
+		if(sale ==0) return (int) Math.round(payPrice * 0.9);
+		else if(sale ==1) return (int) Math.round(payPrice * 0.93);
+		return -1;
+	}
+	
+	public int payByGift(int sale, int priceRow) {
 		Console.writeln("어떤 금액권을 가지고 계시나요?");
 		int gift = Console.readInt();
-		if(price >= Math.round(gift*0.8)) return gift - price;
+		if(price >= Math.round(gift*0.8)) return gift - payPrice(sale, priceRow);
 		return -1;
 	}
 }
